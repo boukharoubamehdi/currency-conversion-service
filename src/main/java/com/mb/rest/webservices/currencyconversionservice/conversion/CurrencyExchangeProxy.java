b0c86e7a854c7fd5c9a497aa85a8cf494d88832d
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2022-05-16 13:38:02 -0400

    Utilizing client-side load balancing (#6)

--- src/main/java/com/mb/rest/webservices/currencyconversionservice/conversion/CurrencyExchangeProxy.java
+++ src/main/java/com/mb/rest/webservices/currencyconversionservice/conversion/CurrencyExchangeProxy.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 // Typically, we would use the application name of the service that we would want to call
 // In our case the microService that we want to call has the property name:
 // spring.application.name=currency-exchange
-@FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+// @FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+
+// By removing url = "http://localhost:8000", the Feign client will talk to Eureka and pick up the instances
+// of currency-exchange and do load balancing between them.
+// This is what is called client-side load balancing.
+// And this is happening through feign.
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
